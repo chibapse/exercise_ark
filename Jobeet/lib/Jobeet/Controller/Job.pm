6ddfeb401ca8d67029b9b5e030f007dee908550b
chiba-hiroshi <chiba-hiroshi@KYCNT1502-01.local>
2017-02-27 10:53:24 +0900

    6日目終了

--- Jobeet/lib/Jobeet/Controller/Job.pm
+++ Jobeet/lib/Jobeet/Controller/Job.pm
@@ -3,10 +3,37 @@ use Ark 'Controller';
 
 use Jobeet::Models;
 
+# /job/
 sub index :Path {
     my ($self, $c) = @_;
 
     $c->stash->{jobs} = models('Schema::Job');
+}
+
+# /job/{job_token} (詳細)
+sub show :Path :Args(1) {
+    my ($self, $c, $job_token) = @_;
+
+}
+
+# /job/create (新規作成)
+sub create :Local  {
+    my ($self, $c) = @_;    
+}
+
+# TODO 複雑なURLに対応して。。。
+sub job :Chained('/') :PathPart :CaptureArgs(1) {
+    my ($self, $c, $job_token) = @_;
+}
+
+# /job/{job_token}/edit (編集)
+sub edit :Chained('job') :PathPart :Args(0){
+    my ($self, $c) = @_;
+}
+
+# /job/{job_token}/delete (削除)
+sub delete :Chained('job') :PathPart :Args(0) {
+    my ($self, $c) + @_;
 }
 
 1;
